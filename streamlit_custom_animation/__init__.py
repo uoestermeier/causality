50c4189e2dec7d4aac7677c5f983466419228a7a
Uwe Oestermeier <u.oestermeier@iwm-tuebingen.de>
2021-08-17 14:32:48 +0200

    Added missing build files.

--- streamlit_custom_animation/__init__.py
+++ streamlit_custom_animation/__init__.py
@@ -3,14 +3,14 @@ import os
 import streamlit.components.v1 as components
 
 # Now the React interface only accepts an array of 1 or 2 elements.
-_component_func = components.declare_component(
-    "custom_animation",
-    url="http://localhost:3000",
-)
+# _component_func = components.declare_component(
+#     "custom_animation",
+#     url="http://localhost:3000",
+# )
 
-# parent_dir = os.path.dirname(os.path.abspath(__file__))
-# build_dir = os.path.join(parent_dir, "frontend/build")
-# _component_func = components.declare_component("custom_animation", path=build_dir)
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+build_dir = os.path.join(parent_dir, "frontend/build")
+_component_func = components.declare_component("custom_animation", path=build_dir)
    
 
 # Edit arguments sent and result received from React component, so the initial input is converted to an array and returned value extracted from the component
